4675d6124b6ee30a3eca1c399d9991e218361703
Nick <93144005+NiRGEsFY@users.noreply.github.com>
2023-11-06 13:28:17 +0300

    Add practic_9

---
+++ Practic_9/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practic_9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var input = TextBoxLeftInput.Text.Split(' ');
+            foreach (var item in input)
+            {
+                LeftListBox.Items.Add(item);
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var input = TextBoxRight.Text.Split(' ');
+            Stack<string> words = new Stack<string>(input);
+            RightLabel.Content = String.Join(" ", words.ToArray());
+        }
+    }
+}
